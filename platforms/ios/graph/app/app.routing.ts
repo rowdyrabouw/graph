8f559fe0dad8a7a2c02595158812f63c0c1a093b
Rowdy Rabouw <rowdy.rabouw@nn.nl>
2017-07-05 19:36:33 +0200

    Removed hooks, node_modules and platforms

--- platforms/ios/graph/app/app.routing.ts
+++
@@ -1,9 +0,0 @@
-import { GraphComponent } from "./components/graph/graph.component";
-
-export const AppRoutes: any = [
-    { path: "", component: GraphComponent }
-];
-
-export const AppComponents: any = [
-    GraphComponent
-];
